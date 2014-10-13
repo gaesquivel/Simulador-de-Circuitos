9ccc979e8dd6d40d8be5f984d383df945bf22cef
FedericoMeza <crimsom-@hotmail.com>
2014-10-12 21:23:09 -0300

    Creacion de Soluciones
    Hay una carpeta para cada equipo

---
+++ PotatoMathica Solver Engine/PotatoMathica Solver Engine Library/Solver.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PotatoMathica
+{
+    public class Solver
+    {
+       public static double Add(double a, double b)
+       {
+          return a + b;
+       }
+    }
+}
---
+++ PotatoMathica Solver Engine/PotatoMathica Solver Engine Testing/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PotatoMathica;
+
+namespace PotatoMathica_Test
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine( Solver.Add(2, 3) );
+         Console.ReadKey();
+      }
+   }
+}
